3ae8671f8ba387ef2d1091ba2ff3fa1047c9d81a
Nontapat Toei - 6609612046 <andas4v4ge@gmail.com>
2026-02-09 22:04:43 +0700

    add project files

---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+export default function Header() {
+    return (
+        <header >
+            <img src="Earth.png" alt="Earth" />
+        <h2>my travel journal</h2>
+        </header>
+
+    )
+}
